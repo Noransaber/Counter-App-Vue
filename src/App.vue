d79a90f121c7f47b1cd57e31300c97f282cbdc17
Noran S Abdelfattah <noransaber685@gmail.com>
2023-03-03 14:33:31 +0200

    Counter-app using Vue.js

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue';
+let count = ref(0);
+</script>
+
+
+<template>
+  <div class="container">
+    <div class="center">
+    <h4>
+      The Current count is... 
+    </h4>
+    <h1>{{ count }}</h1>
+    <button @click='count++'>+</button>
+    <button @click='count--'>-</button>
+  </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.center{
+  text-align: center;
+}
+</style>
